4f70fbb510f12a9e900b526ff08228a6e10fc97d
yerakulmanov <200103127@stu.sdu.edu.kz>
2022-10-03 10:49:05 +0600

    Add click listener to the button

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -11,6 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateTip() }
+
+    }
+    fun calculateTip(){
 
     }
 }
