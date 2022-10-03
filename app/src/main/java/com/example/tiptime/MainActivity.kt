2429fb66f35505c17e82e1e444a0a1d561f6a732
yerakulmanov <200103127@stu.sdu.edu.kz>
2022-10-03 15:25:33 +0600

    Calculate the tip and round it up

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -23,7 +23,11 @@ class MainActivity : AppCompatActivity() {
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
-
+        var tip = cost * tipPercentage
+        val roundUp = binding.roundUpSwitch.isChecked
+        if(roundUp){
+            tip = kotlin.math.ceil(tip)
+        }
 
     }
 }
