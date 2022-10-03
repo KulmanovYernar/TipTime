21c7878d335f45f9ca9b9c74443e52d3a30ba5fa
yerakulmanov <200103127@stu.sdu.edu.kz>
2022-10-04 01:12:27 +0600

    Learn about null

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -17,7 +17,8 @@ class MainActivity : AppCompatActivity() {
     }
     fun calculateTip(){
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null){return}
         val selectedId = binding.tipOptions.checkedRadioButtonId
         val tipPercentage = when(selectedId){
             R.id.option_twenty_percent -> 0.20
