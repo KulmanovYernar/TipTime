a67f1e6072abd9ba573f58e5c0afd4831d2381fd
yerakulmanov <200103127@stu.sdu.edu.kz>
2022-10-03 15:39:12 +0600

    Format the tip

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.tiptime
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
@@ -28,6 +29,7 @@ class MainActivity : AppCompatActivity() {
         if(roundUp){
             tip = kotlin.math.ceil(tip)
         }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
 
     }
 }
